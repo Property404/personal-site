622b08db1334f2e3726c49582cc179c3684139e2
Property404 <martinez.dagan@gmail.com>
2020-03-06 21:04:06 -0600

    CMS: Security: restrict access to server info panel

--- admin/panels/php_info.php
+++ admin/panels/php_info.php
@@ -1,3 +1,8 @@
 <?php
+$docroot = $_SERVER["DOCUMENT_ROOT"];
+include_once($docroot."/headers/config.php");
+include_once($docroot."/headers/dbal.php");
+include_once($docroot."/headers/session.php");
+Session::restrict();
 phpinfo();
 ?>
