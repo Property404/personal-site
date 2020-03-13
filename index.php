3b094b5e79d594120c0cff319dd919a717198d7d
Property404 <martinez.dagan@gmail.com>
2020-03-12 21:59:31 -0500

    Add referrer policy

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="How many Dagans? Just the one, until the cloner works">
     <meta name="author" content="Dagan Martinez">
+	<meta name="referrer" content="same-origin">
 
     <title>Dagan Martinez - Firmware Developer</title>
 
